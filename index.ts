4900a94f58b9084016ab2ca4cf04e7d79f87bf70
urbrioche <aafulin@gmail.com>
2023-05-25 21:05:18 +0800

    [myFrom] initial hard-coded observable

--- index.ts
+++ index.ts
@@ -1,9 +1,24 @@
 import './style.css';
 
-import { of, map } from 'rxjs';
+import { of, map, Observable } from 'rxjs';
 
-of('World')
-  .pipe(map((name) => `Hello, ${name}!`))
-  .subscribe(console.log);
+// of('World')
+//   .pipe(map((name) => `Hello, ${name}!`))
+//   .subscribe(console.log);
 
 // Open the console in the bottom right to see results.
+
+myFrom().subscribe({
+  next: (value) => console.log(value),
+  complete: () => console.log('myFrom complete'),
+});
+
+function myFrom(): Observable<number> {
+  return new Observable((subscriber) => {
+    subscriber.next(1);
+    subscriber.next(2);
+    subscriber.next(3);
+    subscriber.next(4);
+    subscriber.complete();
+  });
+}
